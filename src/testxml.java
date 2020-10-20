abdcf5cd8014ff9872588a5ec73d6d8dd7a9bba8
sylvain vaure <sylvain.vaure@insa-lyon.fr>
2020-10-20 09:11:53 +0200

    adds resources folder with XML files and relative path in main class

--- src/testxml.java
+++ src/testxml.java
@@ -10,21 +10,21 @@ import java.io.File;
 public class testxml {
   public static void main(String[] args) {
 	  try{  
-		  File file = new File("/Users/thomasburesi/Downloads/smallMap.xml");  
+		  File file = new File("src/resources/smallMap.xml");  
 		  DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();  
 		  DocumentBuilder db = dbf.newDocumentBuilder();  
 		  Document doc = db.parse(file);  
 		  doc.getDocumentElement().normalize();  
 		  System.out.println("Root element: " + doc.getDocumentElement().getNodeName());  
 		  NodeList nodeList = doc.getElementsByTagName("map");  
-		  	for (int i = 0; itr < nodeList.getLength(); i++) {  
-		  		Node node = nodeList.item(itr);  
+		  	for (int i = 0; i < nodeList.getLength(); i++) {  
+		  		Node node = nodeList.item(i);  
 		  		System.out.println("\nNode Name :" + node.getNodeName());  
 		  		if (node.getNodeType() == Node.ELEMENT_NODE){  
 		  			Element eElement = (Element) node;  
-		  			System.out.println("id: "+ eElement.getAttribute("id");  
-		  			System.out.println("Latitude: "+ eElement.getAttribute("latitude");  
-		  			System.out.println("Longitude: "+ eElement.getAttribute("longitude");
+		  			System.out.println("id: "+ eElement.getAttribute("id"));  
+		  			System.out.println("Latitude: "+ eElement.getAttribute("latitude"));  
+		  			System.out.println("Longitude: "+ eElement.getAttribute("longitude"));
 		   
 		  		}  
 		  	}  

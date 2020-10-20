5967031dc0ea052c159d821bd55c149dd3647c19
Matteo Dumont <matteo.dumont@insa-lyon.fr>
2020-10-20 08:59:50 +0200

    Ajout fichier test IHM

---
+++ src/testIHM.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+
+public class testIHM {
+
+	public static void main(String[] args) {
+		
+		//Frame
+		JFrame frame = new JFrame();
+		frame.setTitle("Deliver'IF");
+		frame.setSize(1300,720);
+		frame.setLocation(100,100);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+
+	}
+
+}

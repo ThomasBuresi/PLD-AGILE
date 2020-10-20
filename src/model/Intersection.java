99f559b781f4091b600204b61c0644678aa76def
agatheliguori <agathe.liguori@gmail.com>
2020-10-20 09:23:22 +0200

    Parsing and constructors of intersection

--- src/model/Intersection.java
+++ src/model/Intersection.java
@@ -10,9 +10,31 @@ public class Intersection {
      * Default constructor
      */
     public Intersection() {
+    	longitude = 0.0f;
+    	latitude = 0.0f;
+    	idIntersection=0;
+    	
+    			
+    }
+    
+    /**
+     * constructor
+     */
+    public Intersection(float longitude, float latitude, long idIntersection) {
+    	this.longitude = longitude;
+    	this.latitude = latitude;
+    	this.idIntersection= idIntersection;
+    	
+    			
     }
 
-    /**
+    @Override
+	public String toString() {
+		return "Intersection [longitude=" + longitude + ", latitude=" + latitude + ", idIntersection=" + idIntersection
+				+ "]";
+	}
+
+	/**
      * 
      */
     protected float longitude;

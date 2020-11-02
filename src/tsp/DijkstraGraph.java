18766dfd6502454002b0a3a80ea7568c8a10ba8b
sylvain vaure <sylvain.vaure@insa-lyon.fr>
2020-11-02 15:52:55 +0200

    Implements basic Dijkstra

--- src/tsp/DijkstraGraph.java
+++ src/tsp/DijkstraGraph.java
@@ -61,14 +61,25 @@ public class DijkstraGraph implements Graph {
 				}
 			}
 		}
-		return -1;
+		return NO_EDGE;
 	}
 	
 	private void fillGraph() {
-		edges = new int[2*requestList.getRequests().size()][2*requestList.getRequests().size()];
-		int requestNb = 10;
-		for(int i = 0; i < requestNb; i++) {
-			
+		edges = new int[2*requestList.getListRequests().size()][2*requestList.getListRequests().size()];
+		// (2*n, 2*n+1) : pickup and delivery points for request n
+		int requestNb = requestList.getListRequests().size();
+		for(int i = 0; i < 2*requestNb; i++) {
+			for(int j = 0; j < 2*requestNb; j++) {
+				if(i == j) {
+					edges[i][j] = 0;
+				} else {
+					Request originRequest = requestList.getListRequests().get(i/2);
+					Request destinationRequest = requestList.getListRequests().get(j/2);
+					Intersection origin = (i % 2 == 0) ? originRequest.getPickupAddress() : originRequest.getDeliveryAddress();
+					Intersection destination = (j % 2 == 0) ? destinationRequest.getPickupAddress() : destinationRequest.getDeliveryAddress();
+					edges[i][j] = computeDistance(origin, destination);
+				}
+			}
 		}
 	}
 

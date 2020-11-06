1c2ae49be134c84f9b4dd51baabde8082f128ac6
agatheliguori <agathe.liguori@gmail.com>
2020-11-06 09:48:01 +0100

    Modifications of addresses and add method fill delivery tour

--- src/model/DeliveryTour.java
+++ src/model/DeliveryTour.java
@@ -4,6 +4,10 @@ import java.util.*;
 import com.byteowls.jopencage.JOpenCageGeocoder;
 import com.sun.tools.javac.util.Pair;
 
+import tsp.DijkstraGraph;
+import tsp.TSP;
+import tsp.TSP1;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -21,6 +25,9 @@ public class DeliveryTour {
      */ 
 	List <Pair<Intersection, List<Segment>>> tour;
 	List <String> pickupOrDeliver ; // string qui dit si l'intersection est un point de pickup ou delivery
+	CityMap map ;
+	RequestList reqlist;
+	DijkstraGraph g;
 
     /**
      * Default constructor of <code>DeliveryTour</code>
@@ -39,6 +46,15 @@ public class DeliveryTour {
 		pickupOrDeliver =new ArrayList<String>();
     }
     
+    public DeliveryTour(String filenameMap, String filenameRequest) {
+    	tour = new ArrayList <Pair<Intersection, List<Segment>>>();
+		pickupOrDeliver =new ArrayList<String>();
+		this.map = new CityMap(filenameMap);
+		map.fillMap();
+		this.reqlist = new RequestList(filenameRequest,map);
+		reqlist.fillRequests();
+		this.g = new DijkstraGraph(map,reqlist);
+    }
     /**
      * 
      */
@@ -59,6 +75,7 @@ public class DeliveryTour {
 
 
 	public void affiche() {
+
 		
 		for (Pair<Intersection, List<Segment>> pair: tour) {
 			System.out.println(pair.fst.toString());
@@ -70,12 +87,45 @@ public class DeliveryTour {
 			}
 		}
 	}
+
+	public void fillDeliveryTour() {
+		TSP tsp = new TSP1();
+		tsp.searchSolution(20000, g);
 		
+		this.addDeparture(reqlist.getDeparture());
+		this.addIntersectionDetail("");
+		// on commence a un car on a deja traite le cas du depart
+		for(int l = 1; l < 1+2*reqlist.getListRequests().size(); l++) {
+		//ajouter au delivery tour l'intersection qui correspond au numero de la requete ->
+		int currentsolution=tsp.getSolution(l);	
+		if (currentsolution%2!=0) {
+//			System.out.println("pair");
+//			System.out.println(currentsolution + "   " + (tsp.getSolution(l)/2));
+//			System.out.println("Pickup Address :" + reqlist.getListRequests().get(tsp.getSolution(l)/2).getPickupAddress().getIdIntersection() );
+			this.addIntersectionDetail("Pickup Address");
+			this.addStep(reqlist.getListRequests().get(tsp.getSolution(l)/2 ).getPickupAddress(), g.getSegmentPaths()[tsp.getSolution(l)][tsp.getSolution(l-1)]); // inverser l'ordre??
+		}
+		else {
+//			System.out.println("impair");
+//			System.out.println(currentsolution + "   " + (tsp.getSolution(l)/2 -1));
+//			System.out.println("Delivery Address :" + reqlist.getListRequests().get(tsp.getSolution(l)/2 - 1).getDeliveryAddress().getIdIntersection() );
+			this.addIntersectionDetail("Delivery Address");
+			this.addStep(reqlist.getListRequests().get(tsp.getSolution(l)/2 -1).getDeliveryAddress(), g.getSegmentPaths()[tsp.getSolution(l)][tsp.getSolution(l-1)]); // inverser l'ordre??
+		}
+		//retour au point de départ :
+		
+		}
+		this.addIntersectionDetail("Return to Departure");
+		this.addStep(reqlist.getDeparture(), g.getSegmentPaths()[tsp.getSolution(2*reqlist.getListRequests().size())][tsp.getSolution(0)]); // inverser l'ordre??
+		
+	}
+	
+	
 	public void writeDeliveryTourToFile(String filename) {
 		File file = new File(filename);
         FileWriter fr = null;
         BufferedWriter br = null;
-        JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder("fbedb322032b496e89461ac6473217a4");
+        
 
         try{
             fr = new FileWriter(file);
@@ -84,9 +134,10 @@ public class DeliveryTour {
             br.write("~~~~~~~~~~~ Delivery Roadmap ~~~~~~~~~~~"+ System.getProperty("line.separator"));
             for (Pair<Intersection, List<Segment>> pair: tour) {
             	if (p==0) {
-            		br.write("Departure : " + pair.fst.toAddress(jOpenCageGeocoder) +  System.getProperty("line.separator"));
+            		br.write("Departure : " + pair.fst.getName() +  System.getProperty("line.separator"));
             		br.write (System.getProperty("line.separator"));
             	}
+            	// a refaire
 //    			List<Segment> seg = pair.snd;
 //    			if (seg != null) {
 //    				for (Segment s : seg) {
@@ -95,7 +146,7 @@ public class DeliveryTour {
 //    			}
     			if (p!=0) {
     				br.write("Step " + p +": "+ this.pickupOrDeliver.get(p)+ System.getProperty("line.separator"));
-    				br.write("Address : " + pair.fst.toAddress(jOpenCageGeocoder) + System.getProperty("line.separator"));
+    				br.write("Address : " + pair.fst.getName() + System.getProperty("line.separator"));
     				br.write (System.getProperty("line.separator"));
     			}
     			p++;
@@ -113,9 +164,9 @@ public class DeliveryTour {
 	}
 
 
-public void addIntersectionDetail(String detail) {
-	this.pickupOrDeliver.add(detail);
-}
+	public void addIntersectionDetail(String detail) {
+		this.pickupOrDeliver.add(detail);
+	}
 }
 			
 
